5fc42c5bd68aea6d5d138fe71b4780d6e1c7335c
Angiecienta <angie.centeno@pucp.edu.pe>
2023-03-01 13:20:37 -0500

    arreglando desastres

--- src/main/java/com/qradardemo/app/respository/UsuarioRepository.java
+++ src/main/java/com/qradardemo/app/respository/UsuarioRepository.java
@@ -1,16 +1,22 @@
 package com.qradardemo.app.respository;
-import java.util.Optional;
+ 
+import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 import com.qradardemo.app.model.Usuario;
 
-@Repository
+@Service
 public class UsuarioRepository{
+    JdbcTemplate template;
     public Usuario findByEmail(String email){
-
-        return null;
+        
+        String query = "SELECT id_usuario,fid_cliente,email,password,activo FROM BASE_DATOS.USUARIOS WHERE email="+email;
+        //List<Usuario>listaUsuarios=template.query(query, new BeanPropertyRowMapper<Usuario>(Usuario.class)); 
+		Usuario usuario = (Usuario)template.queryForObject(query,new BeanPropertyRowMapper(Usuario.class)); 
+        return usuario;
     }
 }
 
